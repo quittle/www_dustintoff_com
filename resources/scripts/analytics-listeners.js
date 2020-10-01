e545b12df531d3b59ab2add9e5ba4a6ec31c09f9
Dustin Toff <me@dustintoff.com>
2020-10-01 21:17:38 +0100

    Add more Google Analytics integration

---
+++ resources/scripts/analytics-listeners.js
@@ -0,0 +1,35 @@
+/**
+ * Initializer for the analytics listeners code.
+ */
+function alInit() {
+    // Register event listeners
+    window.addEventListener('hashchange', alOnHashChange);
+    initializeLinkListeners();
+}
+
+/**
+ * Attaches Google Analytics listeners to all links
+ */
+function initializeLinkListeners() {
+    var links = document.querySelectorAll('a');
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function onLinkClick(e) {
+            ga('send', 'event', 'link', 'click', e.currentTarget.href);
+        });
+    }
+}
+
+/**
+ * Handler for the `window.onHashChangeEvent`
+ * @param {!Event=} opt_e The event
+ * @return {boolean} If true, stops the event from being further processed
+ */
+function alOnHashChange(opt_e) {
+    /** @type {string} */ const section = location.hash.substring(1);
+    ga('set', 'page', section);
+    ga('send', 'pageview');
+
+    return false;
+}
+
+alInit();
---
+++ resources/scripts/google-analytics-externs.js
@@ -0,0 +1,2 @@
+/** @type {function(...*)} */
+function ga() {}
